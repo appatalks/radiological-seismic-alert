ae11f81d77d45f0ace51b12d44ebefa9b14c6909
Steven B. <4163156+appatalks@users.noreply.github.com>
2024-11-24 02:27:13 -0600

    Update check_nuclear_events.py

--- src/check_nuclear_events.py
+++ src/check_nuclear_events.py
@@ -48,7 +48,6 @@ def get_nearest_radiation_sample(lat, lon):
         response.raise_for_status()
 
         # Debug: Log the raw response content
-        print(f"[DEBUG] Raw Safecast API call: {response}")
         print(f"[DEBUG] Raw Safecast API Response: {response.text}")
 
         # Parse JSON data
